60fd7b7086ee6c64f037d048d1eba81d57a8634a
Jack Cook <hello@jackcook.nyc>
2017-11-06 21:36:56 -0500

    Added animation when clicking start button

---
+++ main.js
@@ -0,0 +1,39 @@
+$("#begin").click(function() {
+    var originalHeight = parseInt($("#container").css("height"));
+    var originalWidth = parseInt($("#container").css("width"));
+    var originalLeftPadding = parseInt($("#container").css("padding-left"));
+    var originalTopMargin = parseInt($("#container").css("margin-top"));
+
+    var originalBorderRadius = $("#container").css("border-radius");
+    var originalPadding = $("#container").css("padding");
+
+    var fullSize = originalWidth + 2 * originalLeftPadding;
+    var newSize = fullSize * (7 / 6);
+
+    var newBorderRadius = newSize / 2 + "px";
+    var newPadding = (newSize - originalHeight) / 2 + "px " + (newSize - originalWidth) / 2 + "px";
+
+    $("#container").animate({
+        borderRadius: newBorderRadius,
+        padding: newPadding
+    }, 100, function() {
+        $("#container").animate({
+            width: "0",
+            height: "0",
+            padding: "0"
+        }, 300, function() {
+            setTimeout(function() {
+                $("#container").animate({
+                    height: originalHeight,
+                    padding: newPadding,
+                    width: originalWidth
+                }, 300, function() {
+                    $("#container").animate({
+                        borderRadius: originalBorderRadius,
+                        padding: originalPadding
+                    }, 100);
+                });
+            }, 500);
+        });
+    });
+});
